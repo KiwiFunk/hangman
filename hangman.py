60e6408a4e5b43a72c29ac39524dfe8f53f40851
Alex <alex_p97@live.com>
2025-02-13 16:31:46 +0000

    Pseudocode for basic game loop created

---
+++ hangman.py
@@ -0,0 +1,15 @@
+#import packages
+
+#setup game variables
+
+#create a function to chose a word, and display it as a series of underscores
+
+#Start Game Loop while player lives > 0
+
+#create a function to check if a letter is in the word. If it is, update the word with the letter, else, remove a life from the player
+
+#create a function to update the word with the guessed letter. If every letter is guessed, break from loop
+
+#end game console logs. You win/Game Over
+
+#promt user to play again
