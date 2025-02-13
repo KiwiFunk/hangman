2329519ffbd2d6e6d781698e744309c7f71565e5
Alex <alex_p97@live.com>
2025-02-13 18:26:53 +0000

    Started on game variables and set_word func

--- hangman.py
+++ hangman.py
@@ -1,8 +1,25 @@
 #import packages
+import os
+import random
 
 #setup game variables
+words = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape', 'honeydew', 'kiwi', 'lemon', 'mango', 'nectarine', 'orange', 'pear', 'quince', 'raspberry', 'strawberry', 'tangerine', 'ugli', 'watermelon', 'xigua', 'yuzu', 'zucchini']
+lives = 5
+
+word_to_guess = ''
+current_guess = ''
+incorrect_guesses = []
+round = 0
 
 #create a function to chose a word, and display it as a series of underscores
+def set_word():
+    global word_to_guess
+    global current_word
+
+    word_to_guess = random.choice(words)
+    for char in word_to_guess:
+        current_guess.append('_')
+    
 
 #Start Game Loop while player lives > 0
 
