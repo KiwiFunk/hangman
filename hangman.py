9455c077d0ccf8df0b1f45abd746125c922ab116
Alex <alex_p97@live.com>
2025-02-13 18:42:03 +0000

    Created variables for set_word() return result

--- hangman.py
+++ hangman.py
@@ -6,8 +6,6 @@ import random
 words = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape', 'honeydew', 'kiwi', 'lemon', 'mango', 'nectarine', 'orange', 'pear', 'quince', 'raspberry', 'strawberry', 'tangerine', 'ugli', 'watermelon', 'xigua', 'yuzu', 'zucchini']
 lives = 5
 
-word_to_guess = ''
-current_guess = ''
 incorrect_guesses = []
 round = 0
 
@@ -22,8 +20,10 @@ def set_word():
         obfuscated.append('_')
 
     return word, obfuscated
-    
 
+words = set_word()
+target_word = words[0]
+current_guess = words[1]
 
 #Start Game Loop while player lives > 0
 while lives > 0:
@@ -35,9 +35,9 @@ while lives > 0:
 
     player_guess = input("Guess a Letter!")
 
-#create a function to check if a letter is in the word. If it is, update the word with the letter, else, remove a life from the player
+    #create a function to check if a letter is in the word. If it is, update the word with the letter, else, remove a life from the player
 
-#create a function to update the word with the guessed letter. If every letter is guessed, break from loop
+    #create a function to update the word with the guessed letter. If every letter is guessed, break from loop
 
 #end game console logs. You win/Game Over
 
